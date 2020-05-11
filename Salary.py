cb18835a8e514ce54c3e4da91dfa73af88d13a3b
DibyaSadhukhan <60581408+DibyaSadhukhan@users.noreply.github.com>
2020-05-11 08:59:20 +0530

    Assignment 2.3
    This program inputs the hours worked and rate per hour and calculate the pay of the worker

---
+++ Salary.py
@@ -0,0 +1,6 @@
+# This program inputs the hours worked and rate per hour and calculate the pay of the worker 
+
+hrs = input("Enter Hours:")
+rate= input("Enter Rate:")
+grossPay=(float(hrs)*float(rate))
+print("Pay:",grossPay)

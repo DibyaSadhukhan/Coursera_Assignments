71a9d0a3883e34e5fd1f0b3996b4770d86d92bc2
DibyaSadhukhan <60581408+DibyaSadhukhan@users.noreply.github.com>
2020-05-11 09:38:51 +0530

    asignment 4.2

--- FunctionSalary.py
+++ FunctionSalary.py
@@ -4,7 +4,7 @@ Pay should be the normal rate for hours up to 40 and time-and-a-half for the hou
 Put the logic to do the computation of pay in a function called computepay() and use the function to do the computation. 
 The function should return a value. Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75). 
 You should use input to read a string and float() to convert the string to a number. 
-Do not worry about error checking the user input unless you want to - you can assume the user types numbers properly. 
+Do not worry about error checking the user input unless you want to -you can assume the user types numbers properly. 
 Do not name your variable sum or use the sum() function.
 """
 
